46f2eb4904f494cd0f57289f4d3538fe55dc0419
YoniAmikam <42375644+YoniAmikam@users.noreply.github.com>
2018-11-04 02:50:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FlappyDragon
+{
+#if WINDOWS || LINUX
+
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (FlappyGame game = new FlappyGame())
+                game.Run();
+        }
+    }
+#endif
+}
