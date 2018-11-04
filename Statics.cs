08166013a98ebfbbefad7a6f12b29c4938933726
YoniAmikam <42375644+YoniAmikam@users.noreply.github.com>
2018-11-04 02:51:07 +0200

    Add files via upload

---
+++ Statics.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyDragon
+{
+    public class Statics
+    {
+        public static int GAME_WIDTH = 400;
+        public static int GAME_HEIGHT = 600;
+
+        public static String GAME_TITLE = "FlappyDragon";
+
+        public static Random RANDOM = new Random();
+
+        public static GameTime GAMETIME;
+        public static SpriteBatch SPRITEBATCH;
+        public static ContentManager CONTENT;
+        public static GraphicsDevice GRAPHICSDEVICE;
+
+
+        public static Texture2D PIXEL;
+        public static Managers.InputManager INPUT;
+
+        public static bool DEBUG = false;
+    }
+}
