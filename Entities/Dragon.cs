2796f6bfe0461027d6119d75a8a9a94cb16a0010
YoniAmikam <42375644+YoniAmikam@users.noreply.github.com>
2018-11-04 02:49:50 +0200

    Add files via upload

---
+++ Entities/Dragon.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyDragon.Entities
+{
+    public class Dragon
+    {
+        public Texture2D[] Textures;
+        public SoundEffect FalpEffect;
+        public SoundEffect ButtomHitEffect;
+        public float Rotation;
+        public float YSpeed;
+        public int texturePosition;
+        public Vector2 Position;
+
+        public int JumpTimer = 500;
+        public double JumpElapsed = 0;
+
+        public int animTimer = 100;
+        public double animElapsed = 0;
+        public int textureAdd = 1;
+
+
+        public bool canJump = true;
+        public bool dead = false;
+         public Dragon()
+         {
+             Textures = new Texture2D[4];
+             this.Textures[0] = Statics.CONTENT.Load<Texture2D>("Textures/Dragon1");
+             this.Textures[1] = Statics.CONTENT.Load<Texture2D>("Textures/Dragon2");
+             this.Textures[2] = Statics.CONTENT.Load<Texture2D>("Textures/Dragon2");
+             this.Textures[3] = Statics.CONTENT.Load<Texture2D>("Textures/Dragon2");
+             this.FalpEffect = Statics.CONTENT.Load<SoundEffect>("Effects/Flap");
+             this.ButtomHitEffect = Statics.CONTENT.Load<SoundEffect>("Effects/PipeHit");
+             YSpeed = 0;
+             this.Position = new Vector2(150, 300);
+         }
+
+         public void Update() 
+         {
+             YSpeed += 0.2f;
+
+                 JumpElapsed += Statics.GAMETIME.ElapsedGameTime.TotalMilliseconds;
+                 if(JumpElapsed > JumpTimer)
+                 {
+                     canJump = true;
+                     JumpElapsed = 0;
+                 }
+
+                 animElapsed += Statics.GAMETIME.ElapsedGameTime.TotalMilliseconds;
+                 if (animElapsed > animTimer)
+                 {
+                     this.texturePosition += this.textureAdd;
+                     if (this.texturePosition == 2 || this.texturePosition == 0)
+                         this.textureAdd = this.textureAdd * -1;
+                     animElapsed = 0;
+                 }
+
+
+                 if (Statics.INPUT.isKeyPressed(Microsoft.Xna.Framework.Input.Keys.Space) && canJump)
+                 {
+                     YSpeed = -5;
+                     FalpEffect.Play();
+                 }
+
+                 Rotation = (float)Math.Atan2(YSpeed, 7);
+
+                 this.Position.Y += YSpeed;
+                 if (this.Position.Y > 500)
+                 {
+                     dead = true;
+                     this.ButtomHitEffect.Play();
+                 }
+         }
+         public Rectangle Bound { get { return new Rectangle((int)this.Position.X - 20, (int)this.Position.Y - 20, 40, 40); } }
+         public void Draw() 
+         {
+             Statics.SPRITEBATCH.Draw(this.Textures[this.texturePosition], this.Position, null, Color.White, this.Rotation, new Vector2(20, 20), 1f, SpriteEffects.None, 0f);
+
+             //show debug
+             if(Statics.DEBUG)
+             Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.Bound, new Color(1f, 0f, 0f, 0.3f));
+                
+         }
+    }
+}
+     
+
---
+++ Entities/Spike.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyDragon.Entities
+{
+    public class Spike
+    {
+        public Texture2D texture;
+        public Vector2 position;
+
+        public bool scored = false;
+
+        public Spike()
+        {
+            this.texture = Statics.CONTENT.Load<Texture2D>("Textures/Spikes");
+            this.position = new Vector2(420, Statics.RANDOM.Next(-200,10));
+        }
+        public void Update()
+        {
+            this.position.X -= 2f;
+        }
+        public Rectangle TopBound1 { get { return new Rectangle((int)this.position.X, (int)this.position.Y, 55, 235); } }
+        public Rectangle TopBound2 { get { return new Rectangle((int)this.position.X+4, (int)this.position.Y, 48, 260); } }
+        public Rectangle TopBound3 { get { return new Rectangle((int)this.position.X + 10, (int)this.position.Y, 38, 280); } }
+        public Rectangle TopBound4 { get { return new Rectangle((int)this.position.X + 14, (int)this.position.Y, 28, 295); } }
+        public Rectangle TopBound5 { get { return new Rectangle((int)this.position.X + 19, (int)this.position.Y, 18, 310); } }
+        public Rectangle TopBound6 { get { return new Rectangle((int)this.position.X + 24, (int)this.position.Y, 8, 339); } }
+        public Rectangle ButtomBound1 { get { return new Rectangle((int)this.position.X, (int)this.position.Y + 560, 53, 200); } }
+        public Rectangle ButtomBound2 { get { return new Rectangle((int)this.position.X+4, (int)this.position.Y + 530, 45, 342); } }
+        public Rectangle ButtomBound3 { get { return new Rectangle((int)this.position.X+10, (int)this.position.Y + 500, 30, 342); } }
+        public Rectangle ButtomBound4 { get { return new Rectangle((int)this.position.X+18, (int)this.position.Y + 480, 15, 342); } }
+        public Rectangle ButtomBound5 { get { return new Rectangle((int)this.position.X+22, (int)this.position.Y + 460, 8, 342); } }
+
+
+
+        public void Draw()
+        {
+            Statics.SPRITEBATCH.Draw(this.texture, this.position, Color.White);
+
+           if (Statics.DEBUG)
+            {
+                //show debug top
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.TopBound1, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.TopBound2, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.TopBound3, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.TopBound4, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.TopBound5, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.TopBound6, new Color(1f, 0f, 0f, 0.3f));
+                //show debug buttom
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.ButtomBound1, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.ButtomBound2, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.ButtomBound3, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.ButtomBound4, new Color(1f, 0f, 0f, 0.3f));
+                Statics.SPRITEBATCH.Draw(Statics.PIXEL, this.ButtomBound5, new Color(1f, 0f, 0f, 0.3f));
+
+
+           }
+        }
+    }
+}
